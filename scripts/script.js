694bb29a0dfdea3c1962353bca99dc07fdf89cb5
gwnhanane <hanane_pengel@hotmail.com>
2023-11-29 13:02:09 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
-// JavaScript Document
-console.log("hi");
+/*variable open button*/
+var btnOpenMenu = document.querySelector("header > button");
+
+/*Laat button naar kliks luisteren*/
+btnOpenMenu.onclick = openmenufunction;
+
+/*Maakt een class aan*/
+function openmenufunction(){
+    document.querySelector("nav").classList.add("toonMenu"); /*<<<< dit zorgt ervoor dat een klas erbij komt*/
+}
+
